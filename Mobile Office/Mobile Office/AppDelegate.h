1fd1b3ebc9f62b957e2d92ea09065c2d3e09dce6
xiaobaitiao <512326469@qq.com>
2016-10-27 17:47:36 +0800

    创建项目

---
+++ Mobile Office/Mobile Office/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mobile Office
+//
+//  Created by 陈国梁 on 2016/10/27.
+//  Copyright © 2016年 ChuanSanJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mobile Office/Mobile Office/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mobile Office
+//
+//  Created by 陈国梁 on 2016/10/27.
+//  Copyright © 2016年 ChuanSanJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
